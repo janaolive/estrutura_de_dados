3d8243b096c3faf74aef541513cc4afe4a374cf8
Janaina Oliveira <janainapp@gmail.com>
2022-12-06 16:48:18 -0300

    fix: errors

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,35 +5,30 @@ import sys
 def process(path_file, instance):
     """Aqui irá sua implementação"""
     current_path = None
-    for index in range(len(instance)):
-        if instance.search(index)['nome_do_arquivo'] == path_file:
-            current_path = instance.search(index)
+    for item in range(len(instance)):
+        if instance.search(item)["nome_do_arquivo"] == path_file:
+            current_path = instance.search(item)
     if current_path is None:
-        data = txt_importer(path_file)
-        lines = len(data)
-        item = {
-            'nome_do_arquivo': path_file,
-            'qtd_linhas': lines,
-            'linhas_do_arquivo': data
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(txt_importer(path_file)),
+            "linhas_do_arquivo": txt_importer(path_file)
         }
         instance.enqueue(data)
-        sys.stdout.write(f"{item}")
+        sys.stdout.write(f"{data}")
 
 
 def remove(instance):
     """Aqui irá sua implementação"""
-    if not len(instance):
-        sys.stdout.write('Não há elementos\n')
-        return
-    first_item = instance.dequeue()
-    print(first_item)
-    path_file = first_item['nome_do_arquivo']
-    sys.stdout.write(f"Arquivo {path_file} removido com sucesso\n")
+    if len(instance):
+        path_file = instance.dequeue()["nome_do_arquivo"]
+        sys.stdout.write(f"Arquivo {path_file} removido com sucesso\n")
+    sys.stdout.write("Não há elementos\n")
 
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
     try:
-        sys.stdout.write(f"{instance.search(position)}")
+        sys.stdout.write(f"{instance.search(position)}\n")
     except IndexError:
-        sys.stderr.write('Posição inválida\n')
+        sys.stderr.write("Posição inválida\n")
