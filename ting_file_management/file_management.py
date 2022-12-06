b5201f8e4e599ad4df43baa04ad7d7222e57890d
Janaina Oliveira <janainapp@gmail.com>
2022-12-06 14:37:43 -0300

    feat: req 1 e 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if '.txt' not in path_file:
+            return print('Formato inválido', file=sys.stderr)
+        with open(path_file) as file:
+            data = file.read()
+            text_file = [line for line in data.split('\n')]
+            print(text_file)
+            return text_file
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
