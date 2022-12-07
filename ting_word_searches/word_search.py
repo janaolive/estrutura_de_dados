94cc91c671adfb38e5f9230aab7a7ffb4c5c6cc2
Janaina Oliveira <janainapp@gmail.com>
2022-12-07 14:05:42 -0300

    feat: req 6 e 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,41 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    list = []
+    scope = len(instance)
+    for index in range(scope):
+        data = instance.search(index)
+        lines = data["linhas_do_arquivo"]
+        occurrences = []
+        for index in range(len(lines)):
+            if word.lower() in lines[index].lower():
+                occurrences.append(index + 1)
+        if len(occurrences):
+            list.append({
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": [{"linha": line} for line in occurrences]
+            })
+    return list
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    list = []
+    scope = len(instance)
+    for index in range(scope):
+        data = instance.search(index)
+        lines = data["linhas_do_arquivo"]
+        occurrences = []
+        for index in range(len(lines)):
+            if word.lower() in lines[index].lower():
+                occurrences.append({
+                    "linha": index + 1,
+                    "conteudo": lines[index]
+                })
+        if len(occurrences):
+            list.append({
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+    return list
